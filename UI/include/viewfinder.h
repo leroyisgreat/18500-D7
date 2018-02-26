60403b9f5cf2d0efbc5d34a54110fdf8a94a5d39
LeRoy Gary <leroyisgreat@gmail.com>
2018-02-26 00:52:22 -0500

    lol finally got perserved aspect ratio

--- UI/include/viewfinder.h
+++ UI/include/viewfinder.h
@@ -22,7 +22,7 @@ protected:
 	//Override default signal handler:
   bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
 
-  Glib::RefPtr<Gdk::Pixbuf> m_image;
+  Glib::RefPtr<Gdk::Pixbuf> image;
 };
 
 #endif // VIEWFINDER_H
--- UI/src/viewfinder.cpp
+++ UI/src/viewfinder.cpp
@@ -18,31 +18,41 @@ Viewfinder::Viewfinder() {
   // TODO: For now, we're loading a static image to the PixBuf for testing 
   //        purposes. Later we'll stream from a camera device on system.
   try {
-    m_image = Gdk::Pixbuf::create_from_file("resources/bg.jpg");
+    image = Gdk::Pixbuf::create_from_file("resources/bg.jpg");
   } catch(const Gio::ResourceError& ex) {
     std::cerr << "ResourceError: " << ex.what() << std::endl;
   } catch(const Gdk::PixbufError& ex) {
     std::cerr << "PixbufError: " << ex.what() << std::endl;
   }
 
-  //if (m_image) 
-	//	set_size_request(m_image->get_width()/2, m_image->get_height()/2);
+  /*
+  if (image) 
+    ap = image->get_width() /(double) image->get_height();
+		//set_size_request(image->get_width()/2, image->get_height()/2);
+    //*/
 }
 
 Viewfinder::~Viewfinder() {}
 
 bool Viewfinder::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
-  if (!m_image)
+  if (!image)
     return false;
 
   Gtk::Allocation allocation = get_allocation();
-  const int width = allocation.get_width();
-  const int height = allocation.get_height();
+  const int frame_width = allocation.get_width();
+  const int frame_height = allocation.get_height();
+  const int img_width = image->get_width();
+  const int img_height = image->get_height();
 
-  // Draw the image in the middle of the drawing area, or (if the image is
-  // larger than the drawing area) draw the middle part of the image.
-  Gdk::Cairo::set_source_pixbuf(cr, m_image,
-    (width - m_image->get_width())/2, (height - m_image->get_height())/2);
+  // Draw the image in the top right corner, fixed aspect ratio to fit the window
+  double scale = std::min(frame_width /(double) img_width,
+                          frame_height /(double) img_height);
+  Glib::RefPtr<Gdk::Pixbuf> image_scaled = 
+        image->scale_simple(img_width*scale, 
+                            img_height*scale, 
+                            Gdk::INTERP_BILINEAR);
+  Gdk::Cairo::set_source_pixbuf(cr, image_scaled, 0, 0);
+
   cr->paint();
 
   return true;
