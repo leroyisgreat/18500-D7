f5f206fdb4e52091e5fa927d9967b8215f41832e
marcusgreer96 <mgg787@gmail.com>
2018-03-23 14:58:53 -0400

    modified algorithm

--- hdr/hdr.py
+++ hdr/hdr.py
@@ -1,5 +1,137 @@
-import cv2
 import numpy as np
+import cv2
+import random
+
+
+def linearWeight(pixel_value):
+    z_min, z_max = 0., 255.
+    if pixel_value <= (z_min + z_max) / 2:
+        return pixel_value - z_min
+    return z_max - pixel_value
+
+
+def sampleIntensities(images):
+    z_min, z_max = 0, 255
+    num_intensities = z_max - z_min + 1
+    num_images = len(images)
+    intensity_values = np.zeros((num_intensities, num_images), dtype=np.uint8)
+
+    # Find the middle image to use as the source for pixel intensity locations
+    mid_img = images[num_images // 2]
+
+    for i in range(z_min, z_max + 1):
+        rows, cols = np.where(mid_img == i)
+        if len(rows) != 0:
+            idx = random.randrange(len(rows))
+            for j in range(num_images):
+                intensity_values[i, j] = images[j][rows[idx], cols[idx]]
+    return intensity_values
+
+
+def computeResponseCurve(intensity_samples, log_exposures, smoothing_lambda, weighting_function):
+    z_min, z_max = 0, 255
+    intensity_range = 255  # difference between min and max possible pixel value for uint8
+    num_samples = intensity_samples.shape[0]
+    num_images = len(log_exposures)
+
+    # NxP + [(Zmax-1) - (Zmin + 1)] + 1 constraints; N + 256 columns
+    mat_A = np.zeros((num_images * num_samples + intensity_range, num_samples + intensity_range + 1), dtype=np.float64)
+    mat_b = np.zeros((mat_A.shape[0], 1), dtype=np.float64)
+
+    # 1. Add data-fitting constraints:
+    k = 0
+    for i in range(num_samples):
+        for j in range(num_images):
+            z_ij = intensity_samples[i, j]
+            w_ij = weighting_function(z_ij)
+            mat_A[k, z_ij] = w_ij
+            mat_A[k, (intensity_range + 1) + i] = -w_ij
+            mat_b[k, 0] = w_ij * log_exposures[j]
+            k += 1
+
+    # 2. Add smoothing constraints:
+    for z_k in range(z_min + 1, z_max):
+        w_k = weighting_function(z_k)
+        mat_A[k, z_k - 1] = w_k * smoothing_lambda
+        mat_A[k, z_k    ] = -2 * w_k * smoothing_lambda
+        mat_A[k, z_k + 1] = w_k * smoothing_lambda
+        k += 1
+
+    # 3. Add color curve centering constraint:
+    mat_A[k, (z_max - z_min) // 2] = 1
+
+    inv_A = np.linalg.pinv(mat_A)
+    x = np.dot(inv_A, mat_b)
+
+    g = x[0: intensity_range + 1]
+    return g[:, 0]
+
+
+def computeRadianceMap(images, log_exposure_times, response_curve, weighting_function):
+    img_shape = images[0].shape
+    img_rad_map = np.zeros(img_shape, dtype=np.float64)
+
+    num_images = len(images)
+    for i in range(img_shape[0]):
+        for j in range(img_shape[1]):
+            g = np.array([response_curve[images[k][i, j]] for k in range(num_images)])
+            w = np.array([weighting_function(images[k][i, j]) for k in range(num_images)])
+            SumW = np.sum(w)
+            if SumW > 0:
+                img_rad_map[i, j] = np.sum(w * (g - log_exposure_times) / SumW)
+            else:
+                img_rad_map[i, j] = g[num_images // 2] - log_exposure_times[num_images // 2]
+    return img_rad_map
+
+
+def globalToneMapping(image, gamma):
+    image_corrected = cv2.pow(image/255., 1.0/gamma)
+    return image_corrected
+
+
+def intensityAdjustment(image, template):
+    m, n, channel = image.shape
+    output = np.zeros((m, n, channel))
+    for ch in range(channel):
+        image_avg, template_avg = np.average(image[:, :, ch]), np.average(template[:, :, ch])
+        output[..., ch] = image[..., ch] * (template_avg / image_avg)
+
+    return output
+
+
+def computeHDR(images, log_exposure_times, smoothing_lambda=100., gamma=0.6):
+
+    num_channels = images[0].shape[2]
+    hdr_image = np.zeros(images[0].shape, dtype=np.float64)
+
+    for channel in range(num_channels):
+        # Collect the current layer of each input image from the exposure stack
+        layer_stack = [img[:, :, channel] for img in images]
+
+        # Sample image intensities
+        intensity_samples = sampleIntensities(layer_stack)
+
+        # Compute Response Curve
+        response_curve = computeResponseCurve(intensity_samples, log_exposure_times, smoothing_lambda, linearWeight)
+
+        # Build radiance map
+        img_rad_map = computeRadianceMap(layer_stack, log_exposure_times, response_curve, linearWeight)
+
+        # Normalize hdr layer to (0, 255)
+        hdr_image[..., channel] = cv2.normalize(img_rad_map, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    # Global tone mapping
+    image_mapped = globalToneMapping(hdr_image, gamma)
+
+    # Adjust image intensity based on the middle image from image stack
+    template = images[len(images)//2]
+    image_tuned = intensityAdjustment(image_mapped, template)
+
+    # Output image
+    output = cv2.normalize(image_tuned, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    return output.astype(np.uint8)
+
+
 
 
 def readImagesAndTimes():
@@ -47,27 +179,3 @@ if __name__ == '__main__':
   ldrDrago = 3 * ldrDrago
   cv2.imwrite("ldr-Drago.jpg", ldrDrago * 255)
   print("saved ldr-Drago.jpg")
-  
-  # Tonemap using Durand's method obtain 24-bit color image
-  print("Tonemaping using Durand's method ... ")
-  tonemapDurand = cv2.createTonemapDurand(1.5,4,1.0,1,1)
-  ldrDurand = tonemapDurand.process(hdrDebevec)
-  ldrDurand = 3 * ldrDurand
-  cv2.imwrite("ldr-Durand.jpg", ldrDurand * 255)
-  print("saved ldr-Durand.jpg")
-  
-  # Tonemap using Reinhard's method to obtain 24-bit color image
-  print("Tonemaping using Reinhard's method ... ")
-  tonemapReinhard = cv2.createTonemapReinhard(1.5, 0,0,0)
-  ldrReinhard = tonemapReinhard.process(hdrDebevec)
-  cv2.imwrite("ldr-Reinhard.jpg", ldrReinhard * 255)
-  print("saved ldr-Reinhard.jpg")
-  
-  # Tonemap using Mantiuk's method to obtain 24-bit color image
-  print("Tonemaping using Mantiuk's method ... ")
-  tonemapMantiuk = cv2.createTonemapMantiuk(2.2,0.85, 1.2)
-  ldrMantiuk = tonemapMantiuk.process(hdrDebevec)
-  ldrMantiuk = 3 * ldrMantiuk
-  cv2.imwrite("ldr-Mantiuk.jpg", ldrMantiuk * 255)
-  print("saved ldr-Mantiuk.jpg")
-
